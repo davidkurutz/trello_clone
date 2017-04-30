91ec5693029e0f1af8356c9556fdddd2af44a440
David Kurutz <davidkurutz@gmail.com>
2017-04-29 21:15:21 -0400

    initial gruntfile - copied from previous project template

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+ module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower_concat: {
+      all: {
+        dest: 'public/javascripts/vendor/all.js',
+        dependencies: {
+          "underscore": "jquery",
+          "backbone": "underscore"
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          "public/javascripts/vendor/all.js": ["public/javascripts/vendor/all.js"]
+        }
+      }
+    },
+    handlebars: {
+      all:{
+        files: {
+          "public/javascripts/handlebars_templates.js": ["handlebars/**/*.hbs"]
+        },
+        options: {
+          processContent: removeWhitespace,
+          processName: extractFileName,
+          partialRegex: /^par_/
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js',
+            'app.js',
+            'routes/*.js',
+            'public/javascripts/models/*.js',
+            'public/javascripts/collections/*.js',
+            'public/javascripts/views/*.js',
+            'public/javascripts/application.js'
+            ]
+    }
+  });
+
+  [ "grunt-bower-concat",
+    "grunt-contrib-uglify",
+    "grunt-contrib-handlebars",
+    "grunt-contrib-jshint"
+  ].forEach(function(task) {
+    grunt.loadNpmTasks(task);
+  });
+
+  grunt.registerTask("default", ['bower_concat', 'uglify', 'handlebars']);
+};
+
+function removeWhitespace(template) {
+  return template.replace(/ {2,}/mg, "").replace(/\r|\n/mg, "");
+}
+
+function extractFileName(file) {
+  return file.match(/\/(.+)\.hbs$/).pop();
+}
