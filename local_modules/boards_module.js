76bebdd305f9abff993609734f84fc494a661d98
David Kurutz <davidkurutz@gmail.com>
2017-05-02 00:51:11 -0400

    backend logic for adding a board

--- local_modules/boards_module.js
+++ local_modules/boards_module.js
@@ -5,7 +5,7 @@ var file_path = path.resolve(path.dirname(__dirname), 'data/boards.json');
 
 module.exports = {
   get: function(id) {
-    var boards = this.getJSON();
+    var boards = this.getData();
 
     if (id) {
       console.log(_.findWhere(boards, {id: id}));
@@ -15,29 +15,46 @@ module.exports = {
     }
   },
   getJSON: function() {
-    return JSON.parse(fs.readFileSync(file_path, "utf8")).data
+    return JSON.parse(fs.readFileSync(file_path, "utf8"));
+  },
+  getData: function() {
+    return this.getJSON().data;
+  },
+  getCurrentId: function() {
+    return this.getJSON().currentId;
   },
   write: function(data) {
     fs.writeFileSync(file_path, JSON.stringify(data), "utf8");
   },
-  set: function(data) {
-    this.write({ data: data })
+  set: function(data, id) {
+    id = id ? id++ : this.getCurrentId();
+    this.write({ data: data, currentId: +id});
   },
   addBoard: function(board) {
-    var id = item.id;
     var boards = this.get();
+    var id = this.getCurrentId();
 
-    boards.push(board)
+    var obj = {
+      id: id,
+      createdBy: 1,
+      createdOn: new Date(),
+      starred: false,
+      open: true,
+      archived: false
+    };
 
-    this.set(boards);
+    _.extend(board, obj);
+    boards.push(board);
+    this.set(boards, id + 1);
+    return board;
   },
   removeBoard: function(board) {
     var id = item.id;
     var boards = this.get();
     var existing_b = _.findWhere(boards, {id: id});
 
-    boards = _.without(boards, existing_b)
+    boards = _.without(boards, existing_b);
 
-    this.set(boards)
+    this.set(boards);
   }
-}
+};
