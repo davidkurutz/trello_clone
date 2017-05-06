3495859624697d10b4d4001e04a3ff7b2c069bf0
David Kurutz <davidkurutz@gmail.com>
2017-05-06 10:00:25 -0400

    rename of board and card to b and c

--- routes/all.js
+++ routes/all.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var route_files = ['boards', 'board', 'lists', 'cards', 'card'];
+var route_files = ['b', 'c', 'boards','cards', 'lists'];
 
 for (var i = 0; i < route_files.length; i++) {
   require(path.resolve(path.dirname(__dirname), "routes/" + route_files[i]))(router);
