43cbb1b7157940d41b89f433e5f669116259d563
David Kurutz <davidkurutz@gmail.com>
2017-05-05 23:45:46 -0400

    added cards route

--- routes/board.js
+++ routes/board.js
@@ -17,29 +17,6 @@ module.exports = function(router) {
     });
   });
 
-  // router.put('/boards/:board_id', function(req, res) {
-  //   var data = req.body;
-  //   var id = +req.params.board_id;
-  //   var boards = Boards.get();
-  //   var board = _.findWhere(boards, {id: id});
-  //   var obj = {};
-
-  //   data.starred = data.starred === "true";
-    
-  //   if (data.starred) {
-  //     data.starredOrder = Boards.getCurrentStarredOrder();
-  //     obj.currentStarredOrder = data.starredOrder + 1;
-  //   } else {
-  //     delete board.starredOrder;
-  //   }
-
-  //   _.extend(board, data);
-  //   obj.data = boards;
-  //   Boards.set(obj);
-
-  //   res.json(board);
-  // });
-
   router.post('/b', function(req, res) {
     var data = req.body;
     var board = Boards.addBoard(data);
---
+++ routes/card.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var _ = require('underscore');
+var Cards = require(path.resolve(path.dirname(__dirname), 'local_modules/cards_module'));
+var Lists = require(path.resolve(path.dirname(__dirname), 'local_modules/lists_module'));
+var Boards = require(path.resolve(path.dirname(__dirname), 'local_modules/boards_module'));
+
+module.exports = function(router) {
+  router.get('/c/:card_id*?', function(req, res) {
+    var cardId = +req.params.card_id;
+    var card = Cards.get(cardId);
+    var listId = card.list_id;
+    var list = Lists.get(listId);
+    var boardId = +list.board_id;
+    var board = Boards.get(boardId);
+
+    res.render('card', {
+      title: "Boards | Trello",
+      boards: Boards.get(),
+      board_id: boardId,
+      lists: Lists.getByBoardId(boardId),
+      card: card
+    });
+  });
+};
