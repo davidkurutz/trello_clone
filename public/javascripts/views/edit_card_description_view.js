6a8202045848be81e53b872c9feae7c8f735fe73
David Kurutz <davidkurutz@gmail.com>
2017-05-06 15:52:48 -0400

    initial commit of view

---
+++ public/javascripts/views/edit_card_description_view.js
@@ -0,0 +1,29 @@
+var EditCardDescriptionView = Backbone.View.extend({
+  template: App.templates.edit_card_description,
+  id: 'edit_description_form',
+  events: {
+    'submit form': 'submit'
+  },
+  submit: function(e) {
+    e.preventDefault();
+    var data = this.$("form").serializeArray();
+    var obj = {};
+
+    data.forEach(function(input) {
+      obj[input.name] = input.value
+    });
+
+    this.model.save(obj, {
+      context: this,
+      success: function(json) {
+        this.remove()
+      }
+    })
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+  },
+  initialize: function() {
+    this.render();
+  }
+});
