7332bc470ec0e57ca29251413355a8d962b3d587
David Kurutz <davidkurutz@gmail.com>
2017-05-07 12:09:57 -0400

    fleshed out board menu item view

--- public/javascripts/views/board_menu_item_view.js
+++ public/javascripts/views/board_menu_item_view.js
@@ -1,9 +1,18 @@
 var BoardMenuItemView = Backbone.View.extend({
   template: App.templates.board_menu_item,
+  tagName: 'li',
+  events: {
+    'click span': 'toggleStarred'
+  },
+  toggleStarred: function(e) {
+    this.model.set('starred', !this.model.get("starred"));
+    this.model.save();
+  },
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
   },
   initialize: function() {
     this.render();
+    this.listenTo(this.model, "change", this.render);
   }
 });
