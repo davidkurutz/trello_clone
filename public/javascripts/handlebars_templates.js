8165e61f828c20724bb96d8b6712052d01651914
David Kurutz <davidkurutz@gmail.com>
2017-05-08 00:56:47 -0400

    handlebars

--- public/javascripts/handlebars_templates.js
+++ public/javascripts/handlebars_templates.js
@@ -144,11 +144,13 @@ this["JST"]["list_actions"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"ma
 },"useData":true});
 
 this["JST"]["list"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<header><div contenteditable=\"true\" class=\"edit_title\">"
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
-    + "</div><span class=\"icon-sm icon-overflow-menu-horizontal list_actions\"></span></header><ul class=\"cards\"></ul><footer><a href=\"#\">Add a card...</a></footer>";
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</div><span class=\"icon-sm icon-overflow-menu-horizontal list_actions\"></span></header><ul class=\"cards\" id='cards-"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "'></ul><footer><a href=\"#\">Add a card...</a></footer>";
 },"useData":true});
 
 this["JST"]["move_all"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
