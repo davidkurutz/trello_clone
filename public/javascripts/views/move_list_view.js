8a077ed137df1fdeacd3a297cc2ba51baa8c7a32
David Kurutz <davidkurutz@gmail.com>
2017-06-18 11:29:01 -0400

    refactor of render for move_list_view

--- public/javascripts/views/move_list_view.js
+++ public/javascripts/views/move_list_view.js
@@ -1,7 +1,23 @@
 var MoveListView = BaseView.extend({
+  id: 'move_list',
   template: App.templates.move_list,
+  events: {
+    'click #select_board': 'showBoardSelect'
+  },
+  showBoardSelect: function(e) {
+    e.stopPropagation();
+    var $db = this.$("#destination_board")
+    $db.show();
+    $db.attr('size', $db.find('option').length + 1);
+  },
   render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
+    this.$el.html(this.template( {
+      model: this.model.toJSON(),
+      board_name: App.Board.get("title"),
+      position: this.model.get('sort_order') + 1,
+      boards: App.Boards.toJSON().map(function(b) { return {id: b.id, title: b.title } })
+    }
+    ));
   },
   initialize: function() {
     this.render();
